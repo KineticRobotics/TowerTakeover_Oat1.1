c8ffe5d8d4aa842670a8de9a1b37c983d4b990e9
KineticRobotics <51425642+KineticRobotics@users.noreply.github.com>
2019-12-09 15:27:30 -0600

    RED side autonomous. 4 cube in unprotected zone.

--- src/autonomous.cpp
+++ src/autonomous.cpp
@@ -5,38 +5,83 @@ void autonomous()
 {
 	dr4bL2.setBrakeMode(AbstractMotor::brakeMode::hold);
   dr4bR2.setBrakeMode(AbstractMotor::brakeMode::hold);
-  strafe2.setBrakeMode(AbstractMotor::brakeMode::hold);
+  strafe2.setBrakeMode(AbstractMotor::brakeMode::coast);
   dr4bL2.setReversed(true);
   right2.setReversed(true);
   
-  //releaseClaw();
-  //pros::delay(100);
-  polynomial(8, 70);
   claw2.moveVelocity(127);
-  pros::delay(300);
-  //claw2.moveVelocity(0);
-  dr4bControlA(28);
-  polynomial(26, 70);
-  dr4bControl2(-5.0);
-  pros::delay(400);
-  claw2.moveVelocity(-127);
-  pros::delay(300);
-  dr4bControl2(-21.0);
-  pros::delay(1000);
-  claw2.moveVelocity(127);
-  pros::delay(1000);
-  claw2.moveVelocity(30);
-  polyTurnReverse(103, 150, 1.0);
-  dr4bControl2(12.0);
-  pros::delay(500);
-  polynomial(42.0, 175);
-  dr4bControl2(-3.0);
-  pros::delay(300);
-  claw2.moveVelocity(-127);
-  pros::delay(300);
-  polynomialB(20, 150);
+  pros::delay(150);
   claw2.moveVelocity(0);
+  polynomial(5.0, 80);
+  dr4bControlA(9.0);
+  pros::delay(350);
+  polynomial(11.0, 75);
+  dr4bControl2(-2.0);
+  pros::delay(100);
+  claw2.moveVelocity(-127);
+  pros::delay(300);
+  dr4bControlA(-7.0);
+  pros::delay(300);
+  claw2.moveVelocity(127);
+  pros::delay(800);
+  claw2.moveVelocity(31);
+  dr4bControlA(10.0);
+  pros::delay(300);
+  polynomial(6.0, 70);
 
+  dr4bControl2(-2.0);
+  pros::delay(100);
+  claw2.moveVelocity(-127);
+  pros::delay(300);
+  dr4bControlA(-7.0);
+  pros::delay(300);
+  claw2.moveVelocity(127);
+  pros::delay(800);
+  claw2.moveVelocity(31);
+  dr4bControlA(10.0);
+  pros::delay(300);
+  polynomial(6.0, 70);
+
+  dr4bControl2(-2.0);
+  pros::delay(100);
+  claw2.moveVelocity(-127);
+  pros::delay(300);
+  dr4bControlA(-7.0);
+  pros::delay(300);
+  claw2.moveVelocity(127);
+  pros::delay(800);
+  claw2.moveVelocity(31);
+  dr4bControlA(5.0);
+  pros::delay(300);
+
+  polynomialB(22.0, 140);
+  polyTurn(135, 90, 1.0);
+  polynomial(19.0, 120);
+  dr4bControl2(-5.0);
+  pros::delay(300);
+  claw2.moveVelocity(-127);
+  pros::delay(300);
+  polynomialB(15.0, 100);
+  claw2.moveVelocity(0);
+  /*claw2.moveVelocity(-127);
+  pros::delay(120);
+  claw2.moveVelocity(0);
+  dr4bControl2(-8.0);
+  pros::delay(1000);
+  claw2.moveVelocity(127);
+  pros::delay(200);
+  claw2.moveVelocity(36);
+  dr4bControlA(7.0);
+  pros::delay(150);*/
+
+  /*polynomial(6.0, 75);
+  claw2.moveVelocity(-127);
+  pros::delay(120);
+  dr4bControl2(-7.0);
+  pros::delay(200);
+  claw2.moveVelocity(127);
+  pros::delay(200);
+  claw2.moveVelocity(30);*/
 
 
 
